0f9939a0d2716e476b238798caa7f52310c0e1f2
Shreya Basu <shreyab4@illinois.edu>
2021-01-28 14:22:02 -0600

    Added CameraX dependencies and Launcher Activity

--- app/src/main/java/com/example/emotiondetector/LaunchActivity.java
+++ app/src/main/java/com/example/emotiondetector/LaunchActivity.java
@@ -2,14 +2,15 @@ package com.example.emotiondetector;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LaunchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_launch);
     }
-
 }
