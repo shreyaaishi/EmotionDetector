84bf8177c74274dee2dccbf5375cc1109886ef1b
Uditha <uvv2@illinois.edu>
2021-02-27 13:03:43 -0800

    Textview constraints and hidden textView until button pres

--- app/src/main/java/com/example/emotiondetector/MainActivity.java
+++ app/src/main/java/com/example/emotiondetector/MainActivity.java
@@ -1,15 +1,16 @@
 package com.example.emotiondetector;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.emotiondetector.ml.Model;
 
@@ -39,6 +40,14 @@ public class MainActivity extends AppCompatActivity {
         ImageView image = (ImageView) findViewById(R.id.imageDisplay);
         image.setImageBitmap(bmp);
 
+        Button button = (Button) findViewById(R.id.analyzeButton);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                TextView tv = MainActivity.this.findViewById(R.id.textView4);
+                tv.setVisibility(View.VISIBLE);
+            }
+        });
+
     }
 
     public void returnLaunch(View view) {
@@ -56,9 +65,17 @@ public class MainActivity extends AppCompatActivity {
             Model model = Model.newInstance(view.getContext());
             Model.Outputs outputs = model.process(tfImage);
             List<Category> probability = outputs.getProbabilityAsCategoryList();
+            float maxprobability= probability.get(0).getScore();
+            int maxIndex= 0;
             for (int i = 0; i < probability.size(); i++) {
+                if(probability.get(i).getScore()>maxprobability){
+                    maxprobability= probability.get(i).getScore();
+                    maxIndex= i;
+                }
                 Log.e("category", String.valueOf(probability.get(i)));
             }
+            TextView textView= findViewById(R.id.textView4);
+            textView.setText(probability.get(maxIndex).getLabel());
 
         } catch(IOException e) {
             Log.e("MainActivity", "IOException");
